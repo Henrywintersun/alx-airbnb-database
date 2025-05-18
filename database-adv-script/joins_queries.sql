7f1320908e809c21616fb306f4018d11cd3bb60b
Henrywintersun <90670176+Henrywintersun@users.noreply.github.com>
2025-05-18 17:11:34 -0400

    Update joins_queries.sql
    okay now commit

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -101,3 +101,13 @@ FROM
 RIGHT JOIN bookings ON users.user_id = bookings.user_id;
 
 
+-- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews.
+SELECT 
+    properties.property_id,
+    properties.property_name,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
